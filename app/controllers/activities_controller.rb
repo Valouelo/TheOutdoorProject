0ee168da6643cb5283f830d3ef16122de12d724a
valentine guy <valentineguy@yahoo.fr>
2024-06-18 15:00:46 +0200

    filters done

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,18 @@
 class ActivitiesController < ApplicationController
   def index
     @activities = Activity.all
+
+    if params[:start_date].present? && params[:end_date].present?
+      @activities = @activities.where('start_date >= ? AND end_date <= ?', params[:start_date], params[:end_date])
+    end
+
+    if params[:num_people].present?
+      @activities = @activities.where('num_people >= ?', params[:num_people])
+    end
+
+    if params[:level].present?
+      @activities = @activities.where(level: params[:level])
+    end
   end
 
   def index_with_map
@@ -31,6 +43,12 @@ class ActivitiesController < ApplicationController
 
   def destroy
     @activity.destroy
-    redirect_to activity_index(@activity), notice: 'Activitie was successfully destroyed.'
+    redirect_to activity_index(@activity), notice: 'Activity was successfully destroyed.'
+  end
+
+  private
+
+  def booking_params
+    params.require(:activity).permit(:title, :description, :start_date, :end_date, :num_people, :level, :photo)
   end
 end
