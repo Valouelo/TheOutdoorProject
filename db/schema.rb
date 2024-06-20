1428c465cb63b5a39224afce7968e64d06c91ec8
valentine guy <valentineguy@yahoo.fr>
2024-06-20 12:42:11 +0200

    indentation index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_19_084944) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_19_174712) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -90,6 +90,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_19_084944) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "activity_id", null: false
+    t.index ["activity_id"], name: "index_reviews_on_activity_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
@@ -113,5 +115,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_19_084944) do
   add_foreign_key "activities", "users"
   add_foreign_key "bookings", "activities"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "activities"
   add_foreign_key "reviews", "users"
 end
