d42b8e86283858e7edc525b3c9c1d0e53ed63108
valentine guy <valentineguy@yahoo.fr>
2024-06-25 16:13:10 +0200

    reviews controller updated

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,57 +3,42 @@ class ReviewsController < ApplicationController
   before_action :set_review, only: %i[show edit update destroy]
   before_action :authenticate_user!, except: %i[index show]
 
-  def index
-    @reviews = @activity.reviews
-  end
+  # def index
+  #   @reviews = @activity.reviews
+  # end
 
-  def show
-    @booking = Booking.find(params[:id])
-    @user = @booking.user
-  end
+  # def show
+  #   @booking = Booking.find(params[:id])
+  #   @user = @booking.user
+  # end
 
   def new
-    @review = @activity.reviews.build
+    @review = Review.new
   end
 
   def create
-    @review = @activity.reviews.build(review_params)
+    @review = Review.new(review_params)
     @review.user = current_user
 
     if @review.save
       redirect_to [@activity, @review], notice: 'Review was successfully created.'
     else
+      flash.now[:alert] = 'Oops, something went wrong! Please try again.'
       render :new
     end
   end
 
-  def edit
-  end
-
-  def update
-    if @review.update(review_params)
-      redirect_to [@activity, @review], notice: 'Review was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @review.destroy
-    redirect_to activity_reviews_path(@activity), notice: 'Review was successfully destroyed.'
-  end
-
   private
 
-  def set_activity
-    @activity = Activity.find(params[:activity_id])
-  end
+  # def set_activity
+  #   @activity = Activity.find(params[:activity_id])
+  # end
 
-  def set_review
-    @review = @activity.reviews.find(params[:id])
-  end
+  # def set_review
+  #   @review = @activity.reviews.find(params[:id])
+  # end
 
   def review_params
-    params.require(:review).permit(:rating, :comment)
+    params.require(:review).permit(:rating, :content)
   end
 end
