e5a4d15b234f91be37a3e63176823b5cc8b50788
LouAnne Euzenot <l.euzenot@gmail.com>
2024-06-27 16:04:59 +0200

    chatbox

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem "jbuilder"
 
 gem "pg_search"
 
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,11 +1,16 @@
 class ChatroomsController < ApplicationController
+  def index
+    @activity = Activity.find(params[:activity_id])
+    @chatrooms = Chatroom.joins(:messages).where(messages: { user_id: current_user.id }).distinct
+
+    render "index"
+  end
+
   def show
     @activity = Activity.find(params[:activity_id])
     @chatroom = @activity.chatroom
     @message = Message.new
-  end
-
-  def index
     @chatrooms = Chatroom.joins(:messages).where(messages: { user_id: current_user.id }).distinct
+    render "index"
   end
 end
